653ae2b3e2571730b2629b39167037f56eb9c79a
mohamed shegow <shegowmo@gmail.com>
2023-02-19 20:56:18 +0100

    working on conditions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,31 +3,23 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ['tag1', 'tag2', 'tag3']
   };
 
+  renderTags(){
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>
 
+    return <ul> {this.state.tags.map(tag => <li key={tag}>{tag}</li>)} </ul>
+  }
 
   render() {
-    let classes = this.getBadgeClasses();
-
     return (
       <div>
-        <span className={classes}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment2</button>
+        {this.renderTags()}
       </div>
     );
   }
 
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += (this.state.count === 0) ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const {count} = this.state
-    return count === 0 ? 'Zero' : count;
-  }
 
 }
 
