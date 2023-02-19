23905be788c2c71aaabb16d6b29a9cdfd131cc17
mohamed shegow <shegowmo@gmail.com>
2023-02-20 00:08:08 +0100

    finshing exercise

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,20 +10,21 @@ class Counter extends Component {
   //   this.handleIncrement = this.handleIncrement.bind(this);
   // }
 
-  handleIncrement = () => {
-    this.setState({count: this.state.count + 1})
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({count: this.state.count + 1});
   };
 
-  doHandleIncrement = () => {
-    this.handleIncrement()
-  }
+  // doHandleIncrement = () => {
+  //   this.handleIncrement({id: 1})
+  // }
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={ () => this.handleIncrement() }
           className="btn btn-secondary btn-sm"
         >
           Increment2
