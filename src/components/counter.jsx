e9356cea1381541e5204758d6473c14d4f1c9e37
mohamed shegow <shegowmo@gmail.com>
2023-02-23 10:45:47 +0100

    adding in grids and logic

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,31 +2,56 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.counter.value
+    value: this.props.counter.value,
   };
 
   handleIncrement = () => {
-    this.setState({value: this.state.value + 1});
+    this.setState({ value: this.state.value + 1 });
   };
 
+  handleDecrease = () => {
+    this.setState({ value: this.state.value - 1 });
+  };
 
   render() {
     return (
       <div>
+        <div class="row m-3 ">
+        <div class="col-sm-1">
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button
-          onClick={this.handleIncrement}
-          className="btn btn-secondary btn-sm"
-        >
-          Increment
-        </button>
-        <button onClick={()=> this.props.onDelete(this.props.counter.id)} className="btn btn-danger btm-sm m-2">Delete</button>
+        </div>
+
+            <div class="col-sm-1">
+              <button
+                onClick={this.handleIncrement}
+                className="btn btn-secondary btn-sm"
+              >
+                +
+              </button>
+            </div>
+            <div class="col-"><button
+              onClick={this.handleDecrease}
+              className="btn btn-secondary btn-sm"
+            >
+              -
+            </button></div>
+            <div class="col-sm-1"><button
+              onClick={() => this.props.onDelete(this.props.counter.id)}
+              className="btn btn-danger btn-sm"
+            >
+              X
+            </button></div>
+
+        </div>
+
+
+
       </div>
     );
   }
 
   getBadgeClasses() {
-    let classes = "badge m-2 badge-";
+    let classes = "badge m-1 badge-";
     classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
